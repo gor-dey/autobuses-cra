b9b026e0b1752e68afc1b9eaac44b203e824be72
gor-dey <gordeychepikov@gmail.com>
2024-06-14 09:35:40 +0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import {MapWidget} from "./components/MapWidget";
-import {FC} from "react";
+import { MapWidget } from "./components/MapWidget";
+import { FC } from "react";
 
 const App: FC = () => {
   return (
-      <MapWidget />
-  )
-}
+    // <MapWidget />
+    // for stop the build
+    <></>
+  );
+};
 
 export default App;
